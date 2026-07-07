1a7360d9bf758e751309018e6d8c756bbaffc51d
Tawich <42299079+tawich@users.noreply.github.com>
2026-07-07 23:20:43 +0300

    Refactor gene expression analysis and visualizationse edits
    Refactor analysis of gene expression data, updating calculations and visualizations. Adjusted data processing steps for clarity and efficiency.

--- Glossina pallidipes _Rhodopsin_RT-qPCR_Analysis.R
+++ Glossina pallidipes _Rhodopsin_RT-qPCR_Analysis.R
@@ -1,3 +1,4 @@
+#Analysis of VISION genes
 getwd()
 setwd("C:/2025/R_Analysis/AnalysisOfGeneExpressionDATA/RT-qPCR_RelativeGeneExpression")
 # Load libraries
@@ -5,47 +6,44 @@ library(dplyr)
 library(ggplot2)
 library(ggpubr)
 library(tidyr)
-library(ggrepel)
 
 # Load data
-dfvg <- read.csv("visualGenes_final.csv")
-# Compute mean HKGs
-dfvg <- dfvg %>%
-  mutate(HK_mean = sqrt(HK1_Ct * HK2_Ct))
+df <- read.csv("visualGenes_final.csv")
+df
+# Compute mean HK
+df <- df %>%
+  mutate(HK_mean = (HK1_Ct + HK2_Ct)/2)
 
 # ΔCt
-dfvg <- dfvg %>%
+df <- df %>%
   mutate(Delta_Ct = Target_Ct - HK_mean)
 
 # Mean ΔCt in control
-control_means <- dfvg %>%
+control_means <- df %>%
   filter(group == "control") %>%
   group_by(goi) %>%
   summarise(mean_Delta_Ct = mean(Delta_Ct))
 
-View(dfvg)
 # ΔΔCt and Fold Change
-dfvg <- dfvg %>%
+df <- df %>%
   left_join(control_means, by="goi") %>%
   mutate(
     DeltaDelta_Ct = Delta_Ct - mean_Delta_Ct,
     FoldChange = 2^(-DeltaDelta_Ct),
     Regulation = ifelse(FoldChange > 1, "Upregulated", "Downregulated")
   )
-#NORMALITY
-#After normality check using shapiro, 1 data not normal hence used wilcoxon/man whitney U test
 
 # Wilcoxon test per gene and group
-genes <- unique(dfvg$goi)
-groups <- unique(dfvg$group)
+genes <- unique(df$goi)
+groups <- unique(df$group)
 groups <- groups[groups != "control"]
 
 results <- data.frame()
 
 for (g in genes) {
   for (trt in groups) {
-    x <- dfvg %>% filter(goi==g, group=="control") %>% pull(Delta_Ct)
-    y <- dfvg %>% filter(goi==g, group==trt) %>% pull(Delta_Ct)
+    x <- df %>% filter(goi==g, group=="control") %>% pull(Delta_Ct)
+    y <- df %>% filter(goi==g, group==trt) %>% pull(Delta_Ct)
     
     if(length(x) >=1 & length(y) >=1){
       wtest <- wilcox.test(x, y)
@@ -69,12 +67,12 @@ for (g in genes) {
   }
 }
 
-# Saving results for use in statistics and putting significant stats
-write.csv(dfvg, "visionGenesResults20082025.csv", row.names=FALSE)
-write.csv(results, "wilcoxonVisionGenesResults20082025.csv", row.names=FALSE)
-getwd()
-# Merging p-values back for plotting
-dfvg_plot <- dfvg %>%
+# Save results
+write.csv(df, "qPCR_relative_expressionFINALBOXPLOT.csv", row.names=FALSE)
+write.csv(results, "qPCR_wilcoxon_resultsFINALBOXPLOT.csv", row.names=FALSE)
+
+# Merge p-values back for plotting
+df_plot <- df %>%
   left_join(
     results %>%
       mutate(group = gsub(" vs Control","",Comparison)) %>%
@@ -82,8 +80,8 @@ dfvg_plot <- dfvg %>%
     by = c("goi"="Gene","group")
   )
 
-# Converting p-value to significance stars
-dfvg_plot <- dfvg_plot %>%
+# Convert p-value to significance stars
+df_plot <- df_plot %>%
   mutate(
     p_star = case_when(
       p_value <=0.001 ~ "***",
@@ -93,8 +91,8 @@ dfvg_plot <- dfvg_plot %>%
     )
   )
 
-# Aggregating Relative Expression per group
-summary_dfvg <- dfvg_plot %>%
+# Aggregate Relative Expression per group/gene
+summary_df <- df_plot %>%
   group_by(goi, group) %>%
   summarise(
     mean_FC = mean(FoldChange),
@@ -102,30 +100,31 @@ summary_dfvg <- dfvg_plot %>%
     p_star=first(p_star)
   )
 
-# Generating boxplots per gene and adding a dotted reference line at 1
+# Barplot per gene WITHOUT error bars, WITHOUT "ns", and WITH dotted reference line at 1
 
-# filtering to keep only rows with significant stars
-summary_dfvg_signif <- summary_dfvg %>%
+# Keep only rows with significant stars
+summary_df_signif <- summary_df %>%
   filter(p_star %in% c("*","**","***"))
 
-# Generating boxplot of individual FoldChange values
+#BOXPLOT
 
-# Get max FoldChange and p_star per gene and group
-dfvg_plot_signif <- dfvg_plot %>%
+# Boxplot of individual FoldChange values with significance stars and dotted line at 1
+
+# Get max FoldChange and p_star per gene and group (only significant)
+df_plot_signif <- df_plot %>%
   filter(p_star %in% c("*","**","***")) %>%
   group_by(goi, group, p_star) %>%
   summarise(
     max_FC = max(FoldChange)
   )
 
-ggplot(dfvg_plot, aes(x=group, y=FoldChange, fill=group)) +
+ggplot(df_plot, aes(x=group, y=FoldChange, fill=group)) +
   geom_boxplot(width=0.6, outlier.shape=NA) +
   geom_jitter(width=0.2, alpha=0.5, size=1) +
   geom_hline(yintercept=1, linetype="dotted", color="black") +
-  
-  # Adding stars for significant comparisons
+  # Add stars for significant comparisons
   geom_text(
-    data=dfvg_plot_signif,
+    data=df_plot_signif,
     aes(label=p_star, y=max_FC + 0.5),
     size=5
   ) +
@@ -139,24 +138,30 @@ ggplot(dfvg_plot, aes(x=group, y=FoldChange, fill=group)) +
   theme_minimal() +
   theme(legend.position="none")
 
-### Focusing ONLY on the analysis on transcripts from G. pallidipes heads with mature trypanosomes: Filtering control and GO+/- from plot and to retain only GM+/+ group
+### FURTHER PLOT EDITS to remove control from plot
+# Filter out 'control' group only for plotting
+df_plot_no_control <- df_plot %>%
+  filter(group != "control")
 
-dfvg_filtered <- dfvg_plot %>% filter(group == "GM+/+")
+# Get significance annotations (excluding control from display but not from stats)
+df_plot_signif <- df_plot_no_control %>%
+  filter(p_star %in% c("*", "**", "***")) %>%
+  group_by(goi, group, p_star) %>%
+  summarise(max_FC = max(FoldChange), .groups = "drop")
 
-ggplot(dfvg_filtered, aes(x = group, y = FoldChange, fill = group)) +
+# Final boxplot
+ggplot(df_plot_no_control, aes(x = group, y = FoldChange, fill = group)) +
   geom_boxplot(width = 0.6, outlier.shape = NA) +
   geom_jitter(width = 0.2, alpha = 0.5, size = 1) +
-  # Label each point with its 'name' in red and smaller font
-  #geom_text_repel(
-  # aes(label = name),
-  #size = 2.5,
-  #color = "black",
-  #max.overlaps = Inf
-  #) +
   geom_hline(yintercept = 1, linetype = "dotted", color = "black") +
+  geom_text(
+    data = df_plot_signif,
+    aes(label = p_star, y = max_FC + 0.5),
+    size = 5
+  ) +
   facet_wrap(~goi, scales = "free_y") +
   labs(
-    title = "Relative Expression for GM+/+ Group",
+    title = "Relative Expression per Gene)",
     y = "Relative Expression (2^-ΔΔCt)",
     x = "Group",
     caption = "* = p≤0.05, ** = p≤0.01, *** = p≤0.001"
@@ -164,49 +169,5 @@ ggplot(dfvg_filtered, aes(x = group, y = FoldChange, fill = group)) +
   theme_minimal() +
   theme(legend.position = "none")
 
-## Filter to remainonly GM+/+ group
-
-dfvg_gm <- dfvg_plot %>% filter(group == "GM+/+")
-
-#Classifying samples into 'high' and 'low' based on 'name'
-dfvg_gm <- dfvg_gm %>%
-  mutate(
-    Parasitemia = case_when(
-      grepl("high", name, ignore.case = TRUE) ~ "High",
-      grepl("low", name, ignore.case = TRUE) ~ "Low",
-      TRUE ~ NA_character_
-    )
-  ) %>%
-  filter(!is.na(Parasitemia))
-
-# Plotting boxplots by Parasitemia
-
-## customize plot aesthetics
-ggplot(dfvg_gm, aes(x = Parasitemia, y = FoldChange, fill = Parasitemia)) +
-  geom_boxplot(width = 0.6, outlier.shape = NA) +
-  geom_jitter(width = 0.2, alpha = 0.6, size = 1.2) +
-  geom_hline(yintercept = 1, linetype = "dotted", color = "blue") +
-  facet_wrap(~goi, scales = "free_y") +
-  labs(
-    title = "Expression Comparison based on Mouthpart parasitemia levels",
-    x = "Genes per Group",
-    y = "Relative Expression (2^-ΔΔCt)"
-  ) +
-  scale_fill_manual(values = c(
-    "Low" = "grey",
-    #"Medium" = "black"(only input this if you are adding this group),
-    "High" = "darkred"
-  )) +
-  theme_minimal() +
-  theme(
-    legend.position = "right",
-    strip.text = element_text(face = "bold", color = "black", size = 10)
-  )
-
-
-##DONE##
-
-
-
-
+#  DONE
 
